f3e85ba78aa0651137f37acf42837003b579a254
Alex Brown <abrowncownow@gmail.com>
2023-02-22 18:03:54 -0800

    basic MERN app built

--- api/server.js
+++ api/server.js
@@ -32,12 +32,12 @@ app.post('/todos/new', (req, res) => {
     res.json(todo);
 })
 
-app.delete('/todos/delete/:id', async (req, res) => {
+app.delete('/todo/delete/:id', async (req, res) => {
     const result = await Todo.findByIdAndDelete(req.params.id);
     res.json(result);
   
 })
-app.put('/todos/complete/:id', async (req, res) => {
+app.get('/todo/complete/:id', async (req, res) => {
     const todo = await Todo.findById(req.params.id);
     todo.complete = !todo.complete;
     todo.save();
